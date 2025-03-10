c97743d7185dac62465e55f3b569fe22015bc804
Amil <amilseferov305@gmail.com>
2025-03-10 17:26:40 +0400

    header added

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AiOutlineMenu } from "react-icons/ai";
+import { FaUserCircle } from "react-icons/fa";
+function Header() {
+  return (
+    <header className='bg-stone-950 flex flex-row justify-between items-center p-[10px] '>
+        <AiOutlineMenu className="text-white  text-[30px] hover:text-[#5c5c5c]"/>
+        <input type='text' placeholder='Search' className='bg-[#5c5c5c]  text-stone-100 outline-none border-none py-[5px] px-[10px] w-[300px] rounded-[11px]' />
+        <FaUserCircle className="text-[32px] text-[#5c5c5c] bg-white rounded-[50%] "/>
+    </header>
+  )
+}
+
+export default Header
