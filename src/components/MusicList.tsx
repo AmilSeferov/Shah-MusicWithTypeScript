f575a18c5bdaf69066d57ee1eab9b2c47e8272e1
Amil <amilseferov305@gmail.com>
2025-03-11 17:13:19 +0400

    musicList scroll buttons added

--- src/components/MusicList.tsx
+++ src/components/MusicList.tsx
@@ -1,15 +1,32 @@
+import { useRef } from "react";
 import MusicCard from "./MusicCard";
-
+import { CiCircleChevLeft } from "react-icons/ci";
+import { CiCircleChevRight } from "react-icons/ci";
 function MusicList() {
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left: 860, behavior: "smooth" });
+    }
+  };
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left:-860, behavior: "smooth" });
+    }
+  };
   return (
-    <div className="flex flex-col w-[100%]" >
-        <div className="flex flex-row justify-between w-[100%] py-[10px]">
-            <h2 className="text-[30px] font-bold">
-            Hip Hop
-            </h2>
-            <button>Diger</button>
+    <div className="flex flex-col w-[100%]">
+      <div className="flex flex-row justify-between w-[100%] py-[10px]">
+        <h2 className="text-[30px] font-bold">Hip Hop</h2>
+        <div className="flex flex-row text-stone-500 text-[30px]">
+          <CiCircleChevLeft  onClick={scrollLeft}/>
+          <CiCircleChevRight onClick={scrollRight} />
         </div>
-      <div className="flex flex-row overflow-x-auto w-[100%] pb-[30px]">
+      </div>
+      <div
+        ref={scrollContainerRef}
+        className="flex flex-row overflow-x-auto  w-[100%] pb-[30px]"
+      >
         <MusicCard />
         <MusicCard />
         <MusicCard />
