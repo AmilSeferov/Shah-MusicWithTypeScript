97f6fd61fd8352940d7ef339320156a0b0e02156
Amil <amilseferov305@gmail.com>
2025-03-12 14:38:40 +0400

    mood card added

---
+++ src/components/MoodCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MoodCard() {
+  return (
+    <div className='flex items-center text-[18px] h-[48px] w-[161px] px-[10px] bg-stone-500 rounded-[5px] border-l-[6px] border-red-600'>MoodCard</div>
+  )
+}
+
+export default MoodCard
--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import DiscoverFilters from '../components/DiscoverFilters'
+import MoodCard from '../components/MoodCard'
 
 function Discover() {
   return <div className=' w-[calc(100%-200px)] px-[120px]'>
-    <DiscoverFilters/>
+    <MoodCard/>
 
     <div className='text-white'></div>
   </div>
