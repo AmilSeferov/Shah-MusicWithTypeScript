27cf06003aac21b032e619c894f7b3df603f526f
Amil <amilseferov305@gmail.com>
2025-03-13 19:45:20 +0400

    autoplay added

--- src/components/AutoPlay.tsx
+++ src/components/AutoPlay.tsx
@@ -3,7 +3,15 @@ import Toggle from "./Toggle";
 
 function AutoPlay() {
   return (
+    <div className="flex items-center w-100% justify-between p-[10px]">
+      <div>
+        <p>Otamatik oynat</p>
+        <p className="text-[12px] text-stone-400">Siranin sonuna benzer sarkilar ekleyin</p>
+      </div>
+      
+<Toggle/>
 
+    </div>
   );
 }
 
