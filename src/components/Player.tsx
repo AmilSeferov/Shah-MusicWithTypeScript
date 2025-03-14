3892e7fb4b78ddc74f2a35746425a2d77afb9e8f
Amil <amilseferov305@gmail.com>
2025-03-14 16:13:19 +0400

    player left part added

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,8 +1,18 @@
+import { IoIosPlay } from "react-icons/io"
+import { IoPlaySkipBackSharp } from "react-icons/io5";
+import { IoPlaySkipForward } from "react-icons/io5";
 
 function Player() {
   return (
-    <div className="w-[100%] absolute bottom-[0] h-[70px] bg-stone-800">player</div>
+    <div className="w-[100%] absolute bottom-[0] py-[20px] bg-stone-800 px-[10px] text-stone-200">
+        <div className="flex items-center w-[320px] text-[24px]">
+        <IoPlaySkipBackSharp />
+        <IoIosPlay className="text-[34px] mx-[10px]"/>
+        <IoPlaySkipForward />
+        <p className="text-[16px] px-[10px]">1:10/2:11</p>
+        </div>
+    </div>
   )
 }
-
+ 
 export default Player
