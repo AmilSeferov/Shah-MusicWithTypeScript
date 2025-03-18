2d415139499ac6e78c96cadfa160e1692c175c9b
Amil <amilseferov305@gmail.com>
2025-03-18 21:19:01 +0400

    mood card responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import MoodList from "../components/MoodList"
 import MusicCard from "../components/MusicCard"
 import MusicList from "../components/MusicList"
 import { RootState } from "../redux/store";
+import MoodCard from "../components/MoodCard";
 
 
 function Home() {
@@ -10,7 +11,7 @@ function Home() {
 
   return (
     <div className={ (menu?'md:w-[calc(100%-200px)]':'') +' text-white w-full sm:w-[calc(100%-70px)]  flex justify-center pt-[20px]'}>
-            <MusicList/>
+            <MoodCard/>
 
     </div>
   )
