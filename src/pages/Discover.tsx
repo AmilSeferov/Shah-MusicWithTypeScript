aef4b504da870e6b43bd0cd71fe99e91fd86a3bd
Amil <amilseferov305@gmail.com>
2025-03-27 17:03:20 +0400

    discoverpage updated

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -10,11 +10,17 @@ import ProfileCard from '../components/ProfileCard'
 
 function Discover() {
   const menu = useSelector((state: RootState) => state.music.menu);
-
+  const data = Object.entries(
+    useSelector((state: RootState) => state.music.data)
+  );
   return <div className={(menu?' w-full md:w-[calc(100%-200px)]':'  sm:w-[calc(100%-70px)]') + '  flex flex-col items-center'}>
    <DiscoverFilters/>
-
+   {data.map((item, index) => {
+        if (item[0].toLowerCase()==='albums'){
+        return <MusicList key={index} data={item} />;}
+      })}
     <div className='text-white'></div>
+    <MoodList/>
   </div>
 }
 
