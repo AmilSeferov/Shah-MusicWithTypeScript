3128ce831a811a21db9dbabe430ec93f7427d9cc
Amil <amilseferov305@gmail.com>
2025-03-28 21:07:23 +0400

    profil function active

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,8 +1,13 @@
+import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router"
+import { addProfile } from "../redux/Slice"
 
 function ProfileCard({data}:{data:any}) {
+  const disPatch=useDispatch()
   return (
-    <Link to={'/profile'}>
+    <Link to={'/profile'} onClick={()=>{
+disPatch(addProfile({name:data?.data.profile.name,img:data?.data.visuals.avatarImage?.sources[0].url}))
+    }}>
     <div className="w-[150px] sm:w-[170px] md:w-[175px] lg:w-[190px] xl:w-[200px] h-fit flex flex-col items-center mr-[10px] ">
         <div className="w-[100%] flex justify-center "><img className="rounded-full w-[100%] h-[150px] sm:h-[170px] md:h-[175px] lg:h-[190px] xl:h-[200px] " src={data?.data.visuals.avatarImage?.sources[0].url?data?.data.visuals.avatarImage?.sources[0].url:"https://lh3.googleusercontent.com/a-/ALV-UjVFEZNLRXQNP8iw2vXja-5C1VSaRH59wP2p4-wxrOJlDExqpqMv=w544-h544-l90-rj" }  alt="" /></div>
         <div className="flex flex-col items-center text-[14px] sm:text-[15px] md:text-[16px]">
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,8 +1,11 @@
+import { useSelector } from "react-redux"
+import { RootState } from "../redux/store"
 
 function ProfilePage() {
+    const data=useSelector((state:RootState)=>state.music.profile)
   return (
     <div className="w-full">
-        <img className="w-full h-[350px] text-center" src="https://lh3.googleusercontent.com/a-/ALV-UjVFEZNLRXQNP8iw2vXja-5C1VSaRH59wP2p4-wxrOJlDExqpqMv=w544-h544-l90-rj" alt="" />
+        <img className="w-full h-[350px] text-center" src={data.img} alt="" />
     </div>
   )
 }
