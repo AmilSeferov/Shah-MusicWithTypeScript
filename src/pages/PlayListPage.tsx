84938dffc8632b052ea9a0bb8a88db9689705160
Amil <amilseferov305@gmail.com>
2025-03-30 17:40:24 +0400

    playlist page responsive

--- src/pages/PlayListPage.tsx
+++ src/pages/PlayListPage.tsx
@@ -11,28 +11,28 @@ import { MdSort } from "react-icons/md";
 function PlayListPage() {
   const data = useSelector((state: RootState) => state.music.playList);
   return (
-    <div className="flex justify-around w-full h-[calc(100vh-160px)] relative items-center">
-      <div className="flex flex-col w-[40%] h-[100%] justify-center  items-center">
-        <img src={data.img} alt="" />
-        <p className="mt-[10px] text-[24px] font-bold ">{data.name}</p>
-        <div className="flex items-center justify-around w-[60%]">
-          <div className="w-[45px] h-[45px] rounded-full bg-neutral-800 flex justify-center items-center">
+    <div className="flex flex-col lg:flex-row justify-around w-full min-h-[100%] lg:h-[calc(100vh-160px)] relative items-center">
+      <div className="flex flex-col w-[40%] lg:h-[100%] justify-center  items-center">
+        <img  src={data.img} alt="" />
+        <p className="mt-[10px] text-[18px] md:text-[24px] font-bold ">{data.name}</p>
+        <div className="flex items-center justify-around w-[100%] lg:w-[60%]">
+          <div className=" h-[30px] w-[30px] lg:w-[45px] lg:h-[45px] rounded-full bg-neutral-800 flex justify-center items-center">
             <FaPen />
           </div>
-          <div className="w-[55px] h-[55px] rounded-full bg-stone-50 flex justify-center items-center">
-            <FaPlay className="text-black text-[20px]  " />
+          <div className="w-[40px] h-[40px] lg:w-[55px] lg:h-[55px] rounded-full bg-stone-50 flex justify-center items-center">
+            <FaPlay className="text-black text-[16px] lg:text-[20px]  " />
           </div>
-          <div className="w-[45px] h-[45px] rounded-full bg-neutral-800 flex justify-center items-center">
+          <div className="h-[30px] w-[30px] lg:w-[45px] lg:h-[45px] rounded-full bg-neutral-800 flex justify-center items-center">
             <SlOptionsVertical />
           </div>
         </div>
       </div>
-      <div className="w-[60%] h-[100%] ">
+      <div className="w-[60%]  lg:h-[100%] ">
         <div className="flex my-[20px]">
           <MdSort className="text-[28px] mr-[10px]" />
           <p>Sirala</p>
         </div>
-        <div className="w-full overflow-auto  h-[100%]">
+        <div className="w-full overflow-auto  lg:h-[100%]">
           <PlayListELement />
           <PlayListELement />
           <PlayListELement />
