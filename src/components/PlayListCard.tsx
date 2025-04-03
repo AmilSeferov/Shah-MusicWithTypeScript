a58f2c5bdb26092862de436873e626a7189a0987
Amil <amilseferov305@gmail.com>
2025-04-03 13:59:12 +0400

    playlist card added

--- src/components/PlayListCard.tsx
+++ src/components/PlayListCard.tsx
@@ -5,46 +5,24 @@ import { addPlayer, sellectMusic } from "../redux/Slice";
 import Option from "./Option";
 import { useEffect, useState } from "react";
 import { RootState } from "../redux/store";
-function MusicCard({ data }: { data: any }) {
+function PlayListCard({ data }: { data: any }) {
   const disPatch = useDispatch();
-  const [visible, setVisible] = useState<boolean>(false);
-  const sellectedMusic = useSelector(
-    (state: RootState) => state.music.selectedMusic
-  );
-  useEffect(() => {
-    // sellectedMusic!={name:data.data.name,img:data.data.coverArt.sources[0].url}&&setVisible(false)
-  }, [sellectedMusic]);
-  console.log(data)
+ 
   return (
     <div
-      onContextMenu={(event) => {
-        event.preventDefault();
-        setVisible(!visible);
-        disPatch(sellectMusic({ name:data.name, img: data.img }))
-      }}
+     
       className="w-fit relative mr-[10px] mb-[10px] md:mr-[10px] lg:mr-[10px]"
     >
-      <Option
-        visible={visible}
-       setvisible={setVisible}
-      />
-      <div className="group relative w-[150px] sm:w-[170px] md:w-[175px] lg:w-[190px] xl:w-[200px]">
+
+      <div className="group relative w-[150px] sm:w-[170px] md:w-[175px] lg:w-[170px] xl:w-[170px]">
         <img
-          className="h-[150px] sm:h-[170px] md:h-[175px] lg:h-[190px] xl:w-[200px]  rounded-[5px] "
+          className="h-[150px] sm:h-[170px] md:h-[175px] lg:h-[170px] xl:w-[170px]  rounded-[5px] "
           src={data.img}
           alt=""
         />
         <Link
-          onClick={() => {
-            disPatch(
-              addPlayer({
-                name: data.name,
-                artist: data.artist,
-                img: data.img,
-              })
-            );
-          }}
-          to={"/player"}
+          
+          to={"/PlayListPage"}
         >
           {" "}
           <FaCirclePlay className=" absolute bottom-[10px] right-[10px] text-[25px] opacity-[0] group-hover:opacity-[1]" />
@@ -67,4 +45,4 @@ function MusicCard({ data }: { data: any }) {
   );
 }
 
-export default MusicCard;
+export default PlayListCard;
--- src/pages/BookCase.tsx
+++ src/pages/BookCase.tsx
@@ -1,83 +1,66 @@
 import MusicCard from "../components/MusicCard";
+import PlayListCard from "../components/PlayListCard";
 
 function BookCase() {
   return (
     <div className="w-[100%] flex justify-center">
       <div className=" w-[90%] flex flex-row flex-wrap ">
-        <MusicCard
+        <PlayListCard
           data={{
             name: "Rock",
             img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
             artist: "Dirty Palm",
           }}
-        />
-         <MusicCard
-          data={{
-            name: "Rock",
-            img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
-            artist: "Dirty Palm",
-          }}
-        /> <MusicCard
+        /><PlayListCard
         data={{
           name: "Rock",
           img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
           artist: "Dirty Palm",
         }}
-      /> <MusicCard
+      /><PlayListCard
       data={{
         name: "Rock",
         img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
         artist: "Dirty Palm",
       }}
-    /> <MusicCard
+    /><PlayListCard
     data={{
       name: "Rock",
       img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
       artist: "Dirty Palm",
     }}
-  /> <MusicCard
+  /><PlayListCard
   data={{
     name: "Rock",
     img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
     artist: "Dirty Palm",
   }}
-/> <MusicCard
+/><PlayListCard
           data={{
             name: "Rock",
             img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
             artist: "Dirty Palm",
           }}
-        /> <MusicCard
+        /><PlayListCard
         data={{
           name: "Rock",
           img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
           artist: "Dirty Palm",
         }}
-      /> <MusicCard
+      /><PlayListCard
       data={{
         name: "Rock",
         img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
         artist: "Dirty Palm",
       }}
-    /> <MusicCard
+    /><PlayListCard
     data={{
       name: "Rock",
       img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
       artist: "Dirty Palm",
     }}
-  /> <MusicCard
-  data={{
-    name: "Rock",
-    img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
-    artist: "Dirty Palm",
-  }}
-/> <MusicCard
-          data={{
-            name: "Rock",
-            img: "https://i.scdn.co/image/ab67616d00001e0235e70dc4e6a0478f98afa7b4",
-            artist: "Dirty Palm",
-          }}
-        />
+  />
+        
       </div>
     </div>
   );
