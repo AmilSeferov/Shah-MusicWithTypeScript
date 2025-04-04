1f62802e67deb4b63d9a15a8e1e453abfc6c5024
Amil <amilseferov305@gmail.com>
2025-04-04 12:41:02 +0400

    additional added playlistpage list

--- src/pages/PlayListPage.tsx
+++ src/pages/PlayListPage.tsx
@@ -10,12 +10,17 @@ import PlayListELement from "../components/PlayListELement";
 import { MdSort } from "react-icons/md";
 function PlayListPage() {
   const data = useSelector((state: RootState) => state.music.playList);
+  const dataAdditional = useSelector(
+    (state: RootState) => state.music.data.tracks.items
+  );
+
+  console.log(dataAdditional);
   return (
     <div className="flex flex-col lg:flex-row justify-around w-full min-h-[100%] lg:h-[calc(100vh-160px)] relative items-center">
       <div className="flex flex-col w-[40%] lg:h-[100%] justify-center  items-center">
-        <img src={data.img?data.img:''} alt="" />
+        <img src={data.img ? data.img : ""} alt="" />
         <p className="mt-[10px] text-[18px] md:text-[24px] font-bold ">
-          {data.name&& data.name}
+          {data.name && data.name}
         </p>
         <div className="flex items-center justify-around w-[100%] lg:w-[60%]">
           <div className=" h-[30px] w-[30px] lg:w-[45px] lg:h-[45px] rounded-full bg-neutral-800 flex justify-center items-center">
@@ -39,7 +44,17 @@ function PlayListPage() {
             <PlayListELement key={index} data={item} />
           ))}
           <p className="w-[90%]  text-[28px] font-bold">Öneriler</p>
-          
+          {dataAdditional.map((item, index) => {
+            return (
+              <PlayListELement
+                key={index}
+                data={{
+                  name: item.data.name,
+                  img: item.data.albumOfTrack.coverArt.sources[0].url,
+                }}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
